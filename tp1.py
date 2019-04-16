d301cfc7adf547066b55486c7dfe2ec09c609e7c
Braian Villasanti <braian.villasanti2@gmail.com>
2019-04-16 20:55:44 -0300

    Arreglamos la ruleta

--- tp1.py
+++ tp1.py
@@ -16,20 +16,29 @@ prob_mut = 0.05
 # lista usando el método de la ruleta.
 def ruleta(f):
     numero = random.uniform(0,1)
+    #print("numero = ", numero)
     fitness_coincidente = 0.0
     for i in range(10):
+        #print("fitness_coincidente = ", fitness_coincidente)
+        if i >= 9:
+            return i
         if numero > fitness_coincidente:
             fitness_coincidente += f[i]
         else:
             return i
 
+# Completa los ceros por delante de una lista argumento para que tenga 30 dígitos
+def completar_ceros(b):
+    #pdb.set_trace()
+    for i in range(30 - len(b)):
+        b.insert(0, '0')
+    return b
 
 # Genera población inicial y la guarda
 for i in range(10):
     x = random.randint(0, (2**30) - 1)
     pob_inicial.append(x) # como números decimales
-    pob_bin.append((list(str(bin(x))))[2:]) # y como números binarios.
-
+    pob_bin.append(completar_ceros((list(str(bin(x))))[2:])) # y como números binarios de 30 dígitos.
     f_obj.append((x/(2**30 - 1))**2) # Llena la tabla de función objetivo.
 
 suma_obj = sum(f_obj)

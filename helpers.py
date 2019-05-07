ec91854e9d035ae00a68210b38a1d0d1cb457a31
Braian Villasanti <braian.villasanti2@gmail.com>
2019-05-06 21:02:30 -0300

    m

--- helpers.py
+++ helpers.py
@@ -18,18 +18,6 @@ def ruleta(f):
         else:
             return i
 
-# Muestra tablas de valores, sumas, promedios, máximos
-def mostrar_tablas(pob_actual, pob_bin, poblacion, f_obj, fitness):
-    print("______________________________________________________________________________________")
-    print("POBLACIÓN", pob_actual, "\t\t\tX\t FUNCIÓN OBJETIVO\tFUNCIÓN FITNESS")
-    for i in range(nro):
-        print(''.join(pob_bin[i]), poblacion[i], f_obj[i], "\t", fitness[i])
-    print("SUMA:\t\t\t\t\t", sum(f_obj), "\t", sum(fitness))
-    print("PROMEDIO:\t\t\t\t", statistics.mean(f_obj) / nro, "\t", statistics.mean(fitness))
-    print("MÁXIMO:\t\t\t\t\t", max(f_obj), "\t", max(fitness))
-    print("______________________________________________________________________________________")
-    print()
-
 # Completa los ceros por delante de una lista argumento para que tenga 30 dígitos
 def completar_ceros(b):
     for i in range(30 - len(b)):
--- tp1.py
+++ tp1.py
@@ -2,30 +2,27 @@
 ## Braian Villasanti, Rafael Verde
 
 import random
-from helpers import ruleta, mostrar_tablas, completar_ceros, crossover, mutar, mostrar_info
-import nro
+from helpers import ruleta, completar_ceros, crossover, mutar, mostrar_info
 import statistics
 import argparse
 
 parser = argparse.ArgumentParser()
-poblacion = []
-pob_bin = []
-f_obj = []
-fitness = []
-suma = 0
+poblacion = [] # Población en números enteros
+pob_bin = [] # Población en números binarios
+f_obj = [] # Resultados de la función objetivo para cada cromosoma
+fitness = [] # Resultados de la función fitness
 prob_cross = 0.75
-prob_mut = 0.05 
-parametro = []
+prob_mut = 0.05
 x = []
 x_minimos = []
 x_promedios= []
 x_maximos = []
+n = 10 # Tamaño de la población
 
 
-#Creación de argumentos para los comandos de cmd
-parser.add_argument("-c", "--crossover", help="Cambia el valor de la probabilidad de crossover", type=float)
-parser.add_argument("-m", "--mutacion", help="Cambia el valor de la probabilidad de mutacion", type=float)
-#parser.add_argument("-p", "--poblacion", help="Cambia el nro de cromosomas en una poblacion", type=int)
+# Creación de argumentos para los comandos de cmd
+parser.add_argument("-c", "--crossover", help="Cambia el valor de la probabilidad de crossover", type=int)
+parser.add_argument("-m", "--mutacion", help="Cambia el valor de la probabilidad de mutación", type=int)
 args = parser.parse_args()
 
 if args.crossover:
@@ -35,33 +32,31 @@ if args.mutacion:
 	prob_mut = args.mutacion
 
 # Genera población inicial y la guarda
-for i in range(nro.n):
+for i in range(n):
     x = random.randint(0, (2**30) - 1)
     poblacion.append(x) # como números decimales
     pob_bin.append(completar_ceros((list(str(bin(x))))[2:])) # y como números binarios de 30 dígitos.
     f_obj.append((x/(2**30 - 1))**2) # Llena la tabla de función objetivo.
 
 # Genera resultados de función fitness.
-for i in range(nro.n):
+for i in range(n):
     fitness.append(f_obj[i] / sum(f_obj))
 
+# Bucle de 200 iteraciones para cada iteración del algoritmo.
 for generacion in range(200):
-    if generacion == 19 or generacion == 99 or generacion == 199:
-        mostrar_tablas(generacion + 1, pob_bin, poblacion, f_obj, fitness)
-
-    resultado_ruleta = []
-    for i in range(nro.n):
+    resultado_ruleta = [] # Lista que guarda los padres que resultarán de la selección.
+    for i in range(n):
         resultado_ruleta.append(pob_bin[ruleta(fitness)])
  
     # Crossover
-    for i in range(0, nro.n-1, 2):
+    for i in range(0, n-1, 2):
         padre = resultado_ruleta[i]
         madre = resultado_ruleta[i + 1]
         punto_cross = random.randint(0,28)
         crossover(pob_bin, padre, madre, punto_cross, prob_cross)
 
     # Mutación
-    for i in range(nro.n):
+    for i in range(n):
         mutar(pob_bin[i], prob_mut)
     
     x_maximos.append(max(f_obj))
@@ -74,15 +69,16 @@ for generacion in range(200):
     fitness = []
     
     # Generar la nueva población en números enteros
-    for i in range(nro.n):
+    for i in range(n):
         poblacion.append(int(''.join(pob_bin[i]), 2))
 
     # Genera resultados de la función objetivo
-    for i in range(nro.n):
+    for i in range(n):
         f_obj.append((poblacion[i]/(2**30 - 1))**2)
 
     # Genera resultados de función fitness.
-    for i in range(nro.n):
+    for i in range(n):
         fitness.append(f_obj[i] / sum(f_obj))
 
+# Mostrar todo en un archivo HTML
 mostrar_info(''.join(max(pob_bin)), x_maximos, x_minimos, x_promedios, prob_cross, prob_mut)
